a312d5423d0fe9571e6b6abbedf8ce4292852165
Tom Moran <32645063+MySupersuit@users.noreply.github.com>
2019-11-15 18:06:46 +0000

    Add files via upload

---
+++ sweng.py
@@ -0,0 +1,147 @@
+from github import Github
+import plotly.express as px
+import plotly.graph_objects as go
+import operator
+
+ownerName = "tensorflow"
+repoName = "tensorflow"
+
+def get_top_n_authors_to_repo(N, repo):
+    authors = {}
+    commits = repo.get_commits()
+#   for commit in commits:
+    for i in range(10000): #range(commits.totalCount):
+        if commits[i].author:
+            if commits[i].author.login in authors:
+                authors[commits[i].author.login] = authors[commits[i].author.login] + 1
+            else:
+                authors[commits[i].author.login] = 1
+    sorted_x = sorted(authors.items(), key=operator.itemgetter(1), reverse=True)
+    nlist = [i[0] for i in sorted_x]
+    print(sorted_x)
+
+    return nlist[:N]
+
+def get_top_n_contributors_to_repo(N, repo):
+    contributors = repo.get_contributors()
+    contributor_list = []
+    j = 0
+    # while i >= 0:
+    #     if repoName not in contributors[j].login:
+    #         if contributors[j].login not in contributor_list:
+    #             contributor_list.append(contributors[j].login)
+    #     i = i - 1
+    #     j += 1
+    for i in range(contributors.totalCount):
+        if repoName not in contributors[i].login:
+            if contributors[i].login not in contributor_list:
+                contributor_list.append(contributors[i].login)
+
+    return contributor_list
+
+
+def initialise_commits_dict(contributors):
+    commits_dict = {}
+    for i in range(len(contributors)):
+        commits_dict[contributors[i]] = []
+
+    return commits_dict
+
+
+def initialise_commits_count_dict(contributors):
+    commits_count_dict = {}
+    for i in range(len(contributors)):
+        commits_count_dict[contributors[i]] = 0
+    return commits_count_dict
+
+
+# N is number of commits from each contributor
+def get_commits_from_top_contributors(N, contributors, repo):
+    commits = repo.get_commits()
+    commits_dict = initialise_commits_dict(contributors)
+    commits_count_dict = initialise_commits_count_dict(contributors)
+    commits_list = []
+    finished = False
+
+    for i in range(commits.totalCount):
+        if commits[i].author:
+            if commits[i].author.login in contributors:
+                if len(commits_dict[commits[i].author.login]) < N:
+                    commits_dict[commits[i].author].append(commits[i])
+                    if commits_finished(commits_dict, N):
+                        break
+    return commits_dict
+
+
+
+
+def commits_finished(commits_dict, N):
+    for key in commits_dict:
+        if len(commits_dict[key]) < N:
+            return False
+    return True
+
+
+def main():
+    g = Github("caac5ca822f0a9d0274f4c2c346e25e85981fb0a")
+    ownerRepo = ownerName + "/" + repoName
+    repo = g.get_repo(ownerRepo)
+    # conts = get_top_n_contributors_to_repo(1000, repo)
+    # for cont in conts:
+    #     print(cont)
+    authrs = get_top_n_authors_to_repo(10, repo)
+    print("sup")
+    for auth in authrs:
+        print(auth)
+    # contributors = get_top_n_contributors_to_repo(2, repo)
+    # commits_dict = get_commits_from_top_contributors(2, conts, repo)
+
+main()
+# commits_list = []
+# x = 0
+# while len(commits_list) < 10:
+#     if commits[x].author:
+#         if commits[x].author.login == contributor_list[0]:
+#             commits_list.append(commits[x])
+#             print(len(commits_list))
+#     x = x + 1
+#
+# impact = 0
+# churn = 0
+# avgChurns = []
+# avgImpacts = []
+#
+# for commit in commits_list:
+#     churnRate = min(commit.stats.additions, commit.stats.deletions) / commit.stats.total
+#     churn = churn + churnRate
+#     impactRate = commit.stats.total
+#     impact = impact + impactRate
+#     print("churn rate ", churnRate)
+#     print(commit.stats.additions)
+#     print(commit.stats.deletions)
+#     print(commit.stats.total)
+#
+# avgImpacts.append(impactRate)
+# avgChurns.append(churnRate)
+#
+# layout = go.Layout(
+#     title="Impact vs Churn",
+#     xaxis=dict(
+#         title="Impact"
+#     ),
+#     yaxis=dict(
+#         title="Churn"
+#     )
+# )
+#
+# fig = go.Figure(layout=layout)
+#
+# fig.add_trace(go.Scatter(
+#     x=impacts,
+#     y=churns,
+#     mode='markers'
+# ))
+#
+# # fig = px.scatter(impacts, x="impacts", y="churns")
+#
+# fig.show()
